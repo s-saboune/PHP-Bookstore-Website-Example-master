becc59c782d18cc7cc5ddb96d6e2d02f4912a153
Salim Saboune <108432219+s-saboune@users.noreply.github.com>
2023-03-25 17:20:58 +0300

    Update index.php

--- PHP-Bookstore-Website-Example-master/bookstore/index.php
+++ PHP-Bookstore-Website-Example-master/bookstore/index.php
@@ -6,9 +6,9 @@
 <?php
 session_start();
 	if(isset($_POST['ac'])){
-		$servername = "localhost";
-		$username = "root";
-		$password = "";
+		$servername = "mysql-server-bookstore-001.mysql.database.azure.com";
+		$username = "ictadmin";
+		$password = "!ct@dmin1234";
 
 		$conn = new mysqli($servername, $username, $password);
 
